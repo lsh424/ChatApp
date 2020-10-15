292acf613ebeacfdcc05dda79cdfe51c888a5ea2
Lee424 <tmdghks424@naver.com>
2020-10-16 06:23:20 +0900

    Create Project

---
+++ ChatApp/ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by seunghwan Lee on 2020/10/16.
+//  Copyright © 2020 LSH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
